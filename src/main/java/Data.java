47ca5b9ffd2c15872e655853ef5507653d0aa2ad
vaas12346 <48239397+vaas12346@users.noreply.github.com>
2019-10-17 00:53:32 +0200

    Update Data.java

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -1,39 +1,38 @@
-
 import java.util.LinkedList;
 import java.util.List;
 
-public class Data {
-    List<String> name1 = new LinkedList<>();
-    List<String> name2 = new LinkedList<>();
-    List<Integer> name3 = new LinkedList<>();
+public class Date {
+    List<String> list1=new LinkedList<>();
+    List<String> list2= new LinkedList<>();
+    List<Integer>list3=new LinkedList<>();
 
-    public Data(List<String> name1, List<String> name2, List<Integer> name3) {
-        this.name1 = name1;
-        this.name2 = name2;
-        this.name3 = name3;
+    public Date(List<String> list1, List<String> list2, List<Integer> list3) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.list3 = list3;
     }
 
-    public List<String> getName1() {
-        return name1;
+    public List<String> getList1() {
+        return list1;
     }
 
-    public void setName1(List<String> name1) {
-        this.name1 = name1;
+    public void setList1(List<String> list1) {
+        this.list1 = list1;
     }
 
-    public List<String> getName2() {
-        return name2;
+    public List<String> getList2() {
+        return list2;
     }
 
-    public void setName2(List<String> name2) {
-        this.name2 = name2;
+    public void setList2(List<String> list2) {
+        this.list2 = list2;
     }
 
-    public List<Integer> getPower() {
-        return name3;
+    public List<Integer> getList3() {
+        return list3;
     }
 
-    public void setPower(List<Integer> power) {
-        this.name3 = name3;
+    public void setList3(List<Integer> list3) {
+        this.list3 = list3;
     }
 }
